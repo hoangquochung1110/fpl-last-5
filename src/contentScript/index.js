9df95ba43f9003737c1108628e3edcbfe1fc5848
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2024-01-11 13:39:49 +0700

    wip

--- src/contentScript/index.js
+++ src/contentScript/index.js
@@ -1,1 +1,10 @@
 console.info('contentScript is running')
+fetch("https://fantasy.premierleague.com/api/element-summary/1/")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data["fixtures"][0]);
+    // Process the retrieved data here
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
